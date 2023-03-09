791b28b755488504d2c74a8828ee643ea4d69402
Benson Wally Tran <bewallyt@gmail.com>
2023-03-08 18:26:07 -0600

    add spline

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Experience1 from './experiences/Experience-1.jsx';
 import Experience2 from './experiences/Experience-2.jsx';
 import Experience3 from './experiences/Experience-3.jsx';
+import Experience4 from './experiences/Experience_4/Experience-4.jsx';
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.querySelector('#root'));
@@ -29,6 +30,11 @@ const Experiences = () => {
           <ListItemText primary="Experience 3" />
         </Link>
       </ListItem>
+      <ListItem>
+        <Link to="/4">
+          <ListItemText primary="Experience 4" />
+        </Link>
+      </ListItem>
     </List>
   );
 };
@@ -40,6 +46,7 @@ root.render(
         <Route key="route-to-experience-1" path="/1" element={<Experience1 />} />
         <Route key="route-to-experience-2" path="/2" element={<Experience2 />} />
         <Route key="route-to-experience-3" path="/3" element={<Experience3 />} />
+        <Route key="route-to-experience-4" path="/4" element={<Experience4 />} />
         <Route key="home" path="/" element={<Experiences />} />
       </Routes>
     </BrowserRouter>
